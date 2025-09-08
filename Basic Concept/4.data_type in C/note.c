fb31c9996e5d8423820d60665f3145e71590bd11
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-09-08 12:07:30 +0545

    data type in c

---
+++ Basic Concept/4.data_type in C/note.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    //printf(format specifier, value of data)
+    printf("This is all about the Data type\n");
+    /* Basic Data: Basic data is that type of data which is easily define without other help. Example of Basic Data type: int, float, character and double*/
+    printf("%d", 123); printf("\n");
+    printf("%c", "A"); printf("\n");
+    printf("%lf", 123.2344); printf("\n");
+    printf("%f",12.22); printf("\n");
+    /* Derived Data: Derived data is that type of data which is define to help of others..*/
+    printf("%s", "Hello Nepal\n");
+    return 0;
+
+}
