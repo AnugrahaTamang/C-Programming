2398ad65996269f057f69f2cceed22281f28bf0e
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-09-08 12:18:27 +0545

    variable

---
+++ Basic Concept/5.Variable/note.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    //Variable: Variable is container which is use to store value of data. 
+    int a = 12334;
+    float b = 12.33;
+    double c = 1244.233;
+    char d = '&';
+    char e[20] = "Hello Anugraha !";
+    printf("%d\n",a);
+    printf("%f\n",b);
+    printf("%lf\n", c);
+    printf("%c\n", d);
+    printf("%s\n", e);
+    return 0;
+}
