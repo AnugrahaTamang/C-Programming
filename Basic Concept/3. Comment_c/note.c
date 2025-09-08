a4006975ccf4159f992a1535c95e0e309c1b1030
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-09-08 10:48:41 +0545

    comment in c

---
+++ Basic Concept/3. Comment_c/note.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    printf("This is single line comment in c -> //");
+    printf("\n");
+    printf("This is the multi line comment in c -> /*  */");
+    //Single line comment
+    /* Multi line comment 
+     which is used to note, documentation and comment*/
+    return 0;
+
+}
