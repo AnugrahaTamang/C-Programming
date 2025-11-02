06218d05d09f776cae9d3398a87e790e91ef77f7
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-11-02 20:12:19 +0545

    boolean

---
+++ Basic Concept/7.Booleans/boolean.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+    bool isprogramming = true;
+    bool isfishtasty = false;
+    printf("%d\n", isprogramming); //output: 1
+    printf("%d\n", isfishtasty); //output: 0
+    return 0;
+}
